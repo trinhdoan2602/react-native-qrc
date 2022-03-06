e8583009ba9b57ae892ab1181392df9177788087
trinhdoan2602 <doancongtrinh6655@gmail.com>
2022-03-06 08:04:53 +0700

    Training React Native

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Scanner from './screens/Scanner'
+import Home from './screens/Home';
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+function App() {
+  return(
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={Home} />
+    <Stack.Screen name="Scanner" component={Scanner} />
+  </Stack.Navigator>
+  );
+}
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  )
+}
